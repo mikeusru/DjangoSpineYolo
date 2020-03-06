4b74e95afbf70501a0b4945523beb028275529da
mikeusru <M.ishkin86>
2020-03-06 13:39:32 -0500

    improved front page view

--- spineyolo/models.py
+++ spineyolo/models.py
@@ -8,6 +8,8 @@ class SpineData(models.Model):
     image = models.ImageField(upload_to="uploaded_spines/images", null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     date_uploaded = models.DateTimeField(auto_now_add=True, null=True)
+    spine_coordinates_file = models.FileField(upload_to="analyzed_spines/coordinates", null=True)
+    analysed_image = models.ImageField(upload_to="analyzed_spines/images", null=True)
 
     def __str__(self):
         return self.image.name
