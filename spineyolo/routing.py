5784a290ca2909c892f4fe6e764831f494c6f98c
mikeusru <M.ishkin86>
2020-03-09 11:41:18 -0400

    temp test chatbox is added and function for a single user

--- spineyolo/routing.py
+++ spineyolo/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/spineyolo/images/analyze_image(?P<pk>\w+)/$', consumers.ChatConsumer),
+    re_path(r'ws/spineyolo/images/analyze/(?P<pk>\w+)/$', consumers.ChatConsumer),
 ]
